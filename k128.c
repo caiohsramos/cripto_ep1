1135cccf99a5e7844df2a9e3b624c59abb65aa1c
Caio Ramos <caioramos97@gmail.com>
2018-04-15 18:19:51 -0300

    Added algorithm files

---
+++ k128.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void enc(char *in_filename, char *out_filename, char *passwd, int erase) {
+	
+}
+
+void dec(char *in_filename, char *out_filename, char *passwd, int erase) {
+	
+}
---
+++ k128.h
@@ -0,0 +1,6 @@
+#ifndef _K128_C_
+#define _K128_C_
+void enc(char *, char *, char *, int );
+void dec(char *, char *, char *, int );
+
+#endif
