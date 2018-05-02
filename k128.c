d9f526dbdc0643c12085de20c5d2343ef85d2328
Caio Ramos <caioramos97@gmail.com>
2018-05-02 10:37:59 -0300

    Added encrypition functions headers

--- k128.c
+++ k128.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "k128.h"
+#include "sbox.h"
 
 void enc(char *in_filename, char *out_filename, char *passwd, int erase) {
 	
@@ -8,3 +10,17 @@ void enc(char *in_filename, char *out_filename, char *passwd, int erase) {
 void dec(char *in_filename, char *out_filename, char *passwd, int erase) {
 	
 }
+
+byte_t *k128(byte_t *in) {
+
+}
+
+byte_t *generate_primearykey(char *key) {
+
+}
+
+byte_t *generate_interkey(byte_t *primary_key, int i) {
+
+}
+
+//byte_t *generate_subkey(
--- k128.h
+++ k128.h
@@ -1,6 +1,20 @@
 #ifndef _K128_C_
 #define _K128_C_
+
+
+
+
+//typedef for byte usage
+typedef unsigned char byte_t;
+//general encryption function
 void enc(char *, char *, char *, int );
+//general decryption function
 void dec(char *, char *, char *, int );
+//128 bit encryption
+byte_t *k128(byte_t *);
+//generate 128 bit key
+byte_t *generate_primearykey(char *);
+//genearte interkey
+byte_t *generate_interkey(byte_t *, int);
 
 #endif
