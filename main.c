267affb426d654215b5771cc17b7ad947c277bcf
Caio Ramos <caioramos97@gmail.com>
2018-05-03 22:54:30 -0300

    Modified to no use argv as args

--- main.c
+++ main.c
@@ -1,21 +1,28 @@
-#include <stdio.h>
 #include "k128.h"
+#include <stdio.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
+	char in[50];
+	char out[50];
+	char passwd[50];
+	strcpy(in, argv[3]);
+	strcpy(out, argv[5]);
+	strcpy(passwd, argv[7]);
 	switch (argv[1][1]) {
 		case 'c':
 			//calls encrypt
 			if(argc == 9 && argv[8][1] == 'a')
-				enc(argv[3], argv[5], argv[7], 1, &k128);
+				enc(in, out, passwd, 1, &k128);
 			else
-				enc(argv[3], argv[5], argv[7], 0, &k128);
+				enc(in, out, passwd, 0, &k128);
 			break;
 		case 'd':
 			//calls decrypt
 			if(argc == 9 && argv[8][1] == 'a')
-				enc(argv[3], argv[5], argv[7], 1, &k128_d);
+				enc(in, out, passwd, 1, &k128_d);
 			else
-				enc(argv[3], argv[5], argv[7], 0, &k128_d);
+				enc(in, out, passwd, 0, &k128_d);
 			break;
 		case '1':
 			//measures randomness method 1
