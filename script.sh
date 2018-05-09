ee84f06d284152df790234a9e43f17222ddc31e6
Caio Ramos <caioramos97@gmail.com>
2018-05-09 10:33:27 -0300

    Modified input image and script

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./k128 -c -i inputs/6ac3f2fc-d7e6-47ea-a0b2-fd25fb0c85eb.jpg -o outputs/1.out -p Bz4cDcLF > /dev/null
+./k128 -c -i inputs/1.jpg -o outputs/1.out -p Bz4cDcLF > /dev/null
 echo "Arquivo criptografado em  outputs/1.out"
 ./k128 -d -i outputs/1.out -o outputs/result.jpg -p Bz4cDcLF > /dev/null
 echo "Arquivo decriptografado em  outputs/result.jpg"
