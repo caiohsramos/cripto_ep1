892eca7313c0722027f0e0c67575072c71c97be8
Caio Ramos <caioramos97@gmail.com>
2018-05-10 18:14:56 -0300

    Changed byte_t for block_t (better)

--- k128.c
+++ k128.c
@@ -5,15 +5,15 @@
 #include <string.h>
 
 //constants
-const byte_t constM = 0xCB3725F7;
-const byte_t constR = 0x13;
+const block_t constM = 0xCB3725F7;
+const block_t constR = 0x13;
 
 void enc(char *in_filename, char *out_filename, char *passwd, int erase) {
-	byte_t *x = (byte_t*)malloc(sizeof(byte_t)*4);
-	byte_t *y = NULL;
-	byte_t *y_old = (byte_t*)malloc(sizeof(byte_t)*4);
+	block_t *x = (block_t*)malloc(sizeof(block_t)*4);
+	block_t *y = NULL;
+	block_t *y_old = (block_t*)malloc(sizeof(block_t)*4);
 	y_old[0] = y_old[1] = y_old[2] = y_old[3] = 0xffffffff;
-	byte_t *key = NULL;
+	block_t *key = NULL;
 	FILE *fp_in = NULL;
 	FILE *fp_out = NULL;
 	long unsigned int file_size, N;
@@ -87,11 +87,11 @@ void enc(char *in_filename, char *out_filename, char *passwd, int erase) {
 }
 
 void dec(char *in_filename, char *out_filename, char *passwd, int erase) {
-	byte_t *x = (byte_t*)malloc(sizeof(byte_t)*4);
-	byte_t *y = NULL;
-	byte_t *x_old = (byte_t*)malloc(sizeof(byte_t)*4);
+	block_t *x = (block_t*)malloc(sizeof(block_t)*4);
+	block_t *y = NULL;
+	block_t *x_old = (block_t*)malloc(sizeof(block_t)*4);
 	x_old[0] = x_old[1] = x_old[2] = x_old[3] = 0xffffffff;
-	byte_t *key = NULL;
+	block_t *key = NULL;
 	FILE *fp_in = NULL;
 	FILE *fp_out = NULL;
 	long unsigned int file_size, N;
@@ -124,7 +124,7 @@ void dec(char *in_filename, char *out_filename, char *passwd, int erase) {
 	y = k128_d(x, x_old, key); 
 	memcpy(x_old, x, 16);
 	//saves the result
-	byte_t *y_tmp = (byte_t*)malloc(sizeof(byte_t)*4);
+	block_t *y_tmp = (block_t*)malloc(sizeof(block_t)*4);
 	memcpy(y_tmp, y, 16);
 	//reads the size block
 	fread(x, 1, 16, fp_in);	
@@ -156,9 +156,9 @@ void dec(char *in_filename, char *out_filename, char *passwd, int erase) {
 	if(erase) remove(in_filename);
 }
 
-byte_t *k128(byte_t *in, byte_t *y_old, byte_t *key) {
+block_t *k128(block_t *in, block_t *y_old, block_t *key) {
 	int i;
-	byte_t *y = (byte_t*)malloc(sizeof(byte_t)*4);
+	block_t *y = (block_t*)malloc(sizeof(block_t)*4);
 	memcpy(y, in, 16);
 	xor(y, y_old);
 	for(i = 0; i < 12; i++) {
@@ -167,9 +167,9 @@ byte_t *k128(byte_t *in, byte_t *y_old, byte_t *key) {
 	return y;
 }
 
-byte_t *k128_d(byte_t *in, byte_t *x_old, byte_t *key) {
+block_t *k128_d(block_t *in, block_t *x_old, block_t *key) {
 	int i;
-	byte_t *y = (byte_t*)malloc(sizeof(byte_t)*4);
+	block_t *y = (block_t*)malloc(sizeof(block_t)*4);
 	memcpy(y, in, 16);
 	for(i = 0; i < 12; i++) {
 		y = iteration_d(11-i, y, key);
@@ -179,9 +179,9 @@ byte_t *k128_d(byte_t *in, byte_t *x_old, byte_t *key) {
 }
 
 
-byte_t *iteration(int i, byte_t *x, byte_t *k) {
-	byte_t aux;
-	byte_t *interk = generate_interkey(k, i);
+block_t *iteration(int i, block_t *x, block_t *k) {
+	block_t aux;
+	block_t *interk = generate_interkey(k, i);
 	x[1] = x[1] ^ f2(x[0],generate_subkey5(0,interk),generate_subkey32(0,interk));
 	x[2] = x[2] ^ f1(x[1],generate_subkey5(1,interk),generate_subkey32(1,interk));
 	x[3] = x[3] ^ f3(x[2],generate_subkey5(2,interk),generate_subkey32(2,interk));
@@ -194,9 +194,9 @@ byte_t *iteration(int i, byte_t *x, byte_t *k) {
 	return x;
 }
 
-byte_t *iteration_d(int i, byte_t *x, byte_t *k) {
-	byte_t aux;
-	byte_t *interk = generate_interkey(k, i);
+block_t *iteration_d(int i, block_t *x, block_t *k) {
+	block_t aux;
+	block_t *interk = generate_interkey(k, i);
 	//make the swaps
 	aux = x[1];
 	x[1] = x[3];
@@ -211,8 +211,8 @@ byte_t *iteration_d(int i, byte_t *x, byte_t *k) {
 	return x;
 }
 
-byte_t *generate_primarykey(char *key) {
-	byte_t *k = (byte_t*)malloc(sizeof(byte_t)*4);
+block_t *generate_primarykey(char *key) {
+	block_t *k = (block_t*)malloc(sizeof(block_t)*4);
 	char x[50];
 	strcpy(x, key);
 	if (strlen(x) < 16) {
@@ -224,9 +224,9 @@ byte_t *generate_primarykey(char *key) {
 	return k;
 }
 
-byte_t *generate_interkey(byte_t *primary_key, int i) {
-	byte_t *k = assign_k0(primary_key);	
-	byte_t aux;
+block_t *generate_interkey(block_t *primary_key, int i) {
+	block_t *k = assign_k0(primary_key);	
+	block_t aux;
 	int j;
 	for(j = 0; j < i; j++) {
 		k[0] = k[0] ^ f2(k[3],_rotl(constR,(((i+2)*(i+2))%3)),_rotl(constM,(((i+2)*(i+2))%7)));
@@ -254,16 +254,16 @@ byte_t *generate_interkey(byte_t *primary_key, int i) {
 
 }
 
-byte_t generate_subkey5(int j, byte_t *interkey) {
+block_t generate_subkey5(int j, block_t *interkey) {
 	return (interkey[(3-j)] & 0x1f);
 }
 
-byte_t generate_subkey32(int j, byte_t *interkey) {
+block_t generate_subkey32(int j, block_t *interkey) {
 	return interkey[j];
 }
 
-byte_t f1(byte_t x, byte_t k5, byte_t k32) {
-	byte_t i, y;
+block_t f1(block_t x, block_t k5, block_t k32) {
+	block_t i, y;
 	unsigned char i1, i2, i3, i4;
 	i = _rotl((k32 ^ x), k5);
 	i1 = get_byte(i, 0);
@@ -276,8 +276,8 @@ byte_t f1(byte_t x, byte_t k5, byte_t k32) {
 	return y;
 }
 
-byte_t f2(byte_t x, byte_t k5, byte_t k32) {
-	byte_t i, y;
+block_t f2(block_t x, block_t k5, block_t k32) {
+	block_t i, y;
 	unsigned char i1, i2, i3, i4;
 	i = _rotl((k32 ^ x), k5);
 	i1 = get_byte(i, 0);
@@ -291,8 +291,8 @@ byte_t f2(byte_t x, byte_t k5, byte_t k32) {
 
 }
 
-byte_t f3(byte_t x, byte_t k5, byte_t k32) {
-	byte_t i, y;
+block_t f3(block_t x, block_t k5, block_t k32) {
+	block_t i, y;
 	unsigned char i1, i2, i3, i4;
 	i = _rotl((k32 ^ x), k5);
 	i1 = get_byte(i, 0);
@@ -306,18 +306,18 @@ byte_t f3(byte_t x, byte_t k5, byte_t k32) {
 
 }
 
-byte_t _rotl(byte_t value, byte_t shift) {
+block_t _rotl(block_t value, block_t shift) {
     if ((shift &= sizeof(value)*8 - 1) == 0)
       return value;
     return (value << shift) | (value >> (sizeof(value)*8 - shift));
 }
 
-unsigned char get_byte(byte_t number, int n) {
+unsigned char get_byte(block_t number, int n) {
 	return ((number >> (8*n)) & 0xff);
 }
 
-byte_t *assign_k0(byte_t *k) {
-	byte_t *k0 = (byte_t*)malloc(sizeof(byte_t)*4);
+block_t *assign_k0(block_t *k) {
+	block_t *k0 = (block_t*)malloc(sizeof(block_t)*4);
 	k0[0] = 0xBD070242 ^ k[0];
 	k0[1] = 0x657B7C8E ^ k[1];
 	k0[2] = 0x874AA67D ^ k[2];
@@ -325,14 +325,14 @@ byte_t *assign_k0(byte_t *k) {
 	return k0;
 }
 
-void xor(byte_t *x, byte_t *y) {
+void xor(block_t *x, block_t *y) {
 	x[0] = x[0]^y[0];
 	x[1] = x[1]^y[1];
 	x[2] = x[2]^y[2];
 	x[3] = x[3]^y[3];
 }
 
-void ones(byte_t *x) {
+void ones(block_t *x) {
 	x[0] = 0xffffffff;
 	x[1] = 0xffffffff;
 	x[2] = 0xffffffff;
--- k128.h
+++ k128.h
@@ -3,7 +3,7 @@
 
 
 //typedef for byte usage
-typedef unsigned int byte_t;
+typedef unsigned int block_t;
 
 //general encryption function
 void enc(char *, char *, char *, int );
@@ -12,42 +12,42 @@ void enc(char *, char *, char *, int );
 void dec(char *, char *, char *, int );
 
 //128 bit encryption
-byte_t *k128(byte_t *, byte_t*, byte_t*);
-byte_t *k128_d(byte_t *, byte_t*, byte_t*);
+block_t *k128(block_t *, block_t*, block_t*);
+block_t *k128_d(block_t *, block_t*, block_t*);
 
 //generate 128 bit key
-byte_t *generate_primarykey(char *);
+block_t *generate_primarykey(char *);
 
 //genearte interkey
-byte_t *generate_interkey(byte_t *, int);
+block_t *generate_interkey(block_t *, int);
 
 //generate 5bit subkey
-byte_t generate_subkey5(int , byte_t *);
+block_t generate_subkey5(int , block_t *);
 
 //generate 32 bit subkey
-byte_t generate_subkey32(int , byte_t *);
+block_t generate_subkey32(int , block_t *);
 
 //funcions that use sbox
-byte_t f1(byte_t , byte_t , byte_t );
-byte_t f2(byte_t , byte_t , byte_t );
-byte_t f3(byte_t , byte_t , byte_t );
+block_t f1(block_t , block_t , block_t );
+block_t f2(block_t , block_t , block_t );
+block_t f3(block_t , block_t , block_t );
 
 //left rotation
-byte_t _rotl(byte_t , byte_t );
+block_t _rotl(block_t , block_t );
 
 //get nth byte of int
-unsigned char get_byte(byte_t , int );
+unsigned char get_byte(block_t , int );
 
 //make the first subkey
-byte_t *assign_k0(byte_t *);
+block_t *assign_k0(block_t *);
 
 //makes an iteration
-byte_t *iteration(int , byte_t *, byte_t* );
-byte_t *iteration_d(int , byte_t *, byte_t* );
+block_t *iteration(int , block_t *, block_t* );
+block_t *iteration_d(int , block_t *, block_t* );
 
-void xor(byte_t*, byte_t*);
+void xor(block_t*, block_t*);
 
-void ones(byte_t *);
+void ones(block_t *);
 
 
 #endif
